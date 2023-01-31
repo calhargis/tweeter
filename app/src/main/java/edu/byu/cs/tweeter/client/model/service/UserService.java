b23f4ce9f4497d75ea31c98023275a28e57f7cb6
calhargis <calhargis1@gmail.com>
2023-01-31 16:18:06 -0700

    Restructured Login Logic

---
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
@@ -0,0 +1,54 @@
+package edu.byu.cs.tweeter.client.model.service;
+
+import edu.byu.cs.tweeter.client.backgroundTask.BackgroundTaskUtils;
+import edu.byu.cs.tweeter.client.backgroundTask.LoginTask;
+import edu.byu.cs.tweeter.client.backgroundTask.handler.LoginTaskHandler;
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.domain.User;
+
+/**
+ * Contains the business logic to support the login operation.
+ */
+public class UserService {
+
+    /**
+     * An observer interface to be implemented by observers who want to be notified when
+     * asynchronous operations complete.
+     */
+    public interface LoginObserver {
+        void handleSuccess(User user, AuthToken authToken);
+        void handleFailure(String message);
+        void handleException(Exception exception);
+    }
+
+    /**
+     * Creates an instance.
+     */
+    public UserService() {
+    }
+
+    /**
+     * Makes an asynchronous login request.
+     *
+     * @param username the user's name.
+     * @param password the user's password.
+     */
+    public void login(String username, String password, LoginObserver observer) {
+        LoginTask loginTask = getLoginTask(username, password, observer);
+        BackgroundTaskUtils.runTask(loginTask);
+    }
+
+    /**
+     * Returns an instance of {@link LoginTask}. Allows mocking of the LoginTask class for
+     * testing purposes. All usages of LoginTask should get their instance from this method to
+     * allow for proper mocking.
+     *
+     * @return the instance.
+     */
+    LoginTask getLoginTask(String username, String password, LoginObserver observer) {
+        return new LoginTask(username, password, new LoginTaskHandler(observer));
+    }
+
+
+
+}
--- app/src/main/java/edu/byu/cs/tweeter/client/view/main/following/FollowingFragment.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/view/main/following/FollowingFragment.java
@@ -1,10 +1,8 @@
 package edu.byu.cs.tweeter.client.view.main.following;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,20 +21,17 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import edu.byu.cs.tweeter.R;
-import edu.byu.cs.tweeter.client.backgroundTask.GetFollowingTask;
-import edu.byu.cs.tweeter.client.backgroundTask.GetUserTask;
 import edu.byu.cs.tweeter.client.cache.Cache;
-import edu.byu.cs.tweeter.client.view.main.MainActivity;
+import edu.byu.cs.tweeter.client.presenter.FollowingPresenter;
+import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
 
 /**
- * Implements the "Following" tab.
+ * The fragment that displays on the 'Following' tab.
  */
-public class FollowingFragment extends Fragment {
+public class FollowingFragment extends Fragment implements FollowingPresenter.View {
 
     private static final String LOG_TAG = "FollowingFragment";
     private static final String USER_KEY = "UserKey";
@@ -44,20 +39,19 @@ public class FollowingFragment extends Fragment {
     private static final int LOADING_DATA_VIEW = 0;
     private static final int ITEM_VIEW = 1;
 
-    private static final int PAGE_SIZE = 10;
-
-    private User user;
+    private FollowingPresenter presenter;
 
     private FollowingRecyclerViewAdapter followingRecyclerViewAdapter;
 
     /**
-     * Creates an instance of the fragment and places the target user in an arguments
+     * Creates an instance of the fragment and places the user and auth token in an arguments
      * bundle assigned to the fragment.
      *
-     * @param user the user whose following is being displayed (not necessarily the logged-in user).
+     * @param user the logged in user.
+//     * @param authToken the auth token for this user's session.
      * @return the fragment.
      */
-    public static FollowingFragment newInstance(User user) {
+    public static FollowingFragment newInstance(User user/*, AuthToken authToken */) {
         FollowingFragment fragment = new FollowingFragment();
 
         Bundle args = new Bundle(1);
@@ -67,12 +61,46 @@ public class FollowingFragment extends Fragment {
         return fragment;
     }
 
+    /**
+     * Called to notify the view when data loading starts and ends.
+     *
+     * @param value true if we are loading, false otherwise.
+     */
+    @Override
+    public void setLoading(boolean value) {
+        followingRecyclerViewAdapter.setLoading(value);
+    }
+
+    /**
+     * Called to pass "following" users to the view when they are loaded.
+     *
+     * @param newUsers list of new "following" users.
+     */
+    @Override
+    public void addItems(List<User> newUsers) {
+        followingRecyclerViewAdapter.addItems(newUsers);
+    }
+
+    /**
+     * Directs the view to display the specified error message to the user.
+     *
+     * @param message error message to be displayed.
+     */
+    @Override
+    public void displayErrorMessage(String message) {
+        Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_following, container, false);
 
-        user = (User) getArguments().getSerializable(USER_KEY);
+        User user = (User) getArguments().getSerializable(USER_KEY);
+
+        AuthToken authToken = Cache.getInstance().getCurrUserAuthToken();
+
+        presenter = new FollowingPresenter(this, user, authToken);
 
         RecyclerView followingRecyclerView = view.findViewById(R.id.followingRecyclerView);
 
@@ -83,6 +111,8 @@ public class FollowingFragment extends Fragment {
         followingRecyclerView.setAdapter(followingRecyclerViewAdapter);
 
         followingRecyclerView.addOnScrollListener(new FollowRecyclerViewPaginationScrollListener(layoutManager));
+
+        presenter.loadMoreItems();
 
         return view;
     }
@@ -101,23 +131,25 @@ public class FollowingFragment extends Fragment {
          *
          * @param itemView the view on which the user will be displayed.
          */
-        FollowingHolder(@NonNull View itemView) {
+        FollowingHolder(@NonNull View itemView, int viewType) {
             super(itemView);
 
-            userImage = itemView.findViewById(R.id.userImage);
-            userAlias = itemView.findViewById(R.id.userAlias);
-            userName = itemView.findViewById(R.id.userName);
+            if(viewType == ITEM_VIEW) {
+                userImage = itemView.findViewById(R.id.userImage);
+                userAlias = itemView.findViewById(R.id.userAlias);
+                userName = itemView.findViewById(R.id.userName);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    GetUserTask getUserTask = new GetUserTask(Cache.getInstance().getCurrUserAuthToken(),
-                            userAlias.getText().toString(), new GetUserHandler());
-                    ExecutorService executor = Executors.newSingleThreadExecutor();
-                    executor.execute(getUserTask);
-                    Toast.makeText(getContext(), "Getting user's profile...", Toast.LENGTH_LONG).show();
-                }
-            });
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(getContext(), "You selected '" + userName.getText() + "'. NOTE: THIS SAMPLE CODE IS A SUBSET OF THE PROJECT FUNCTIONALITY SO THERE'S NOWHERE FOR THIS CLICK TO GO. YOU WILL NEED TO NAVIGATE TO THE CLICKED USER IN YOUR PROJECT.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } else {
+                userImage = null;
+                userAlias = null;
+                userName = null;
+            }
         }
 
         /**
@@ -126,37 +158,9 @@ public class FollowingFragment extends Fragment {
          * @param user the user.
          */
         void bindUser(User user) {
+            Picasso.get().load(user.getImageUrl()).into(userImage);
             userAlias.setText(user.getAlias());
             userName.setText(user.getName());
-            Picasso.get().load(user.getImageUrl()).into(userImage);
-        }
-
-        /**
-         * Message handler (i.e., observer) for GetUserTask.
-         */
-        private class GetUserHandler extends Handler {
-
-            public GetUserHandler() {
-                super(Looper.getMainLooper());
-            }
-
-            @Override
-            public void handleMessage(@NonNull Message msg) {
-                boolean success = msg.getData().getBoolean(GetUserTask.SUCCESS_KEY);
-                if (success) {
-                    User user = (User) msg.getData().getSerializable(GetUserTask.USER_KEY);
-
-                    Intent intent = new Intent(getContext(), MainActivity.class);
-                    intent.putExtra(MainActivity.CURRENT_USER_KEY, user);
-                    startActivity(intent);
-                } else if (msg.getData().containsKey(GetUserTask.MESSAGE_KEY)) {
-                    String message = msg.getData().getString(GetUserTask.MESSAGE_KEY);
-                    Toast.makeText(getContext(), "Failed to get user's profile: " + message, Toast.LENGTH_LONG).show();
-                } else if (msg.getData().containsKey(GetUserTask.EXCEPTION_KEY)) {
-                    Exception ex = (Exception) msg.getData().getSerializable(GetUserTask.EXCEPTION_KEY);
-                    Toast.makeText(getContext(), "Failed to get user's profile because of exception: " + ex.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            }
         }
     }
 
@@ -167,16 +171,18 @@ public class FollowingFragment extends Fragment {
 
         private final List<User> users = new ArrayList<>();
 
-        private User lastFollowee;
-
-        private boolean hasMorePages;
-        private boolean isLoading = false;
-
         /**
-         * Creates an instance and loads the first page of following data.
+         * Called to notify the adapter when data loading starts and ends.
+         *
+         * @param value true if we are loading, false otherwise.
          */
-        FollowingRecyclerViewAdapter() {
-            loadMoreItems();
+        void setLoading(boolean value) {
+            if (value) {
+                addLoadingFooter();
+            }
+            else {
+                removeLoadingFooter();
+            }
         }
 
         /**
@@ -215,10 +221,10 @@ public class FollowingFragment extends Fragment {
         }
 
         /**
-         * Creates a view holder for a followee to be displayed in the RecyclerView or for a message
-         * indicating that new rows are being loaded if we are waiting for rows to load.
+         *  Creates a view holder for a followee to be displayed in the RecyclerView or for a message
+         *  indicating that new rows are being loaded if we are waiting for rows to load.
          *
-         * @param parent   the parent view.
+         * @param parent the parent view.
          * @param viewType the type of the view (ignored in the current implementation).
          * @return the view holder.
          */
@@ -228,14 +234,14 @@ public class FollowingFragment extends Fragment {
             LayoutInflater layoutInflater = LayoutInflater.from(FollowingFragment.this.getContext());
             View view;
 
-            if (viewType == LOADING_DATA_VIEW) {
-                view = layoutInflater.inflate(R.layout.loading_row, parent, false);
+            if(viewType == LOADING_DATA_VIEW) {
+                view =layoutInflater.inflate(R.layout.loading_row, parent, false);
 
             } else {
                 view = layoutInflater.inflate(R.layout.user_row, parent, false);
             }
 
-            return new FollowingHolder(view);
+            return new FollowingHolder(view, viewType);
         }
 
         /**
@@ -243,19 +249,18 @@ public class FollowingFragment extends Fragment {
          * we are loading new data, the display at that position will be the data loading footer.
          *
          * @param followingHolder the ViewHolder to which the followee should be bound.
-         * @param position        the position (in the list of followees) that contains the followee to be
-         *                        bound.
+         * @param position the position (in the list of followees) that contains the followee to be
+         *                 bound.
          */
         @Override
         public void onBindViewHolder(@NonNull FollowingHolder followingHolder, int position) {
-            if (!isLoading) {
+            if(!presenter.isLoading()) {
                 followingHolder.bindUser(users.get(position));
             }
         }
 
         /**
          * Returns the current number of followees available for display.
-         *
          * @return the number of followees available for display.
          */
         @Override
@@ -272,23 +277,7 @@ public class FollowingFragment extends Fragment {
          */
         @Override
         public int getItemViewType(int position) {
-            return (position == users.size() - 1 && isLoading) ? LOADING_DATA_VIEW : ITEM_VIEW;
-        }
-
-        /**
-         * Causes the Adapter to display a loading footer and make a request to get more following
-         * data.
-         */
-        void loadMoreItems() {
-            if (!isLoading) {   // This guard is important for avoiding a race condition in the scrolling code.
-                isLoading = true;
-                addLoadingFooter();
-
-                GetFollowingTask getFollowingTask = new GetFollowingTask(Cache.getInstance().getCurrUserAuthToken(),
-                        user, PAGE_SIZE, lastFollowee, new GetFollowingHandler());
-                ExecutorService executor = Executors.newSingleThreadExecutor();
-                executor.execute(getFollowingTask);
-            }
+            return (position == users.size() - 1 && presenter.isLoading()) ? LOADING_DATA_VIEW : ITEM_VIEW;
         }
 
         /**
@@ -296,7 +285,7 @@ public class FollowingFragment extends Fragment {
          * loading footer view) at the bottom of the list.
          */
         private void addLoadingFooter() {
-            addItem(new User("Dummy", "User", "dummyurl"));
+            addItem(new User("Dummy", "User", ""));
         }
 
         /**
@@ -305,39 +294,6 @@ public class FollowingFragment extends Fragment {
          */
         private void removeLoadingFooter() {
             removeItem(users.get(users.size() - 1));
-        }
-
-
-        /**
-         * Message handler (i.e., observer) for GetFollowingTask.
-         */
-        private class GetFollowingHandler extends Handler {
-
-            public GetFollowingHandler() {
-                super(Looper.getMainLooper());
-            }
-
-            @Override
-            public void handleMessage(@NonNull Message msg) {
-                isLoading = false;
-                removeLoadingFooter();
-
-                boolean success = msg.getData().getBoolean(GetFollowingTask.SUCCESS_KEY);
-                if (success) {
-                    List<User> followees = (List<User>) msg.getData().getSerializable(GetFollowingTask.FOLLOWEES_KEY);
-                    hasMorePages = msg.getData().getBoolean(GetFollowingTask.MORE_PAGES_KEY);
-
-                    lastFollowee = (followees.size() > 0) ? followees.get(followees.size() - 1) : null;
-
-                    followingRecyclerViewAdapter.addItems(followees);
-                } else if (msg.getData().containsKey(GetFollowingTask.MESSAGE_KEY)) {
-                    String message = msg.getData().getString(GetFollowingTask.MESSAGE_KEY);
-                    Toast.makeText(getContext(), "Failed to get following: " + message, Toast.LENGTH_LONG).show();
-                } else if (msg.getData().containsKey(GetFollowingTask.EXCEPTION_KEY)) {
-                    Exception ex = (Exception) msg.getData().getSerializable(GetFollowingTask.EXCEPTION_KEY);
-                    Toast.makeText(getContext(), "Failed to get following because of exception: " + ex.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            }
         }
     }
 
@@ -364,8 +320,8 @@ public class FollowingFragment extends Fragment {
          * indicated that there was more data to load.
          *
          * @param recyclerView the RecyclerView.
-         * @param dx           the amount of horizontal scroll.
-         * @param dy           the amount of vertical scroll.
+         * @param dx the amount of horizontal scroll.
+         * @param dy the amount of vertical scroll.
          */
         @Override
         public void onScrolled(@NotNull RecyclerView recyclerView, int dx, int dy) {
@@ -375,17 +331,19 @@ public class FollowingFragment extends Fragment {
             int totalItemCount = layoutManager.getItemCount();
             int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
 
-            if (!followingRecyclerViewAdapter.isLoading && followingRecyclerViewAdapter.hasMorePages) {
+            if (!presenter.isLoading() && presenter.isHasMorePages()) {
                 if ((visibleItemCount + firstVisibleItemPosition) >=
                         totalItemCount && firstVisibleItemPosition >= 0) {
                     // Run this code later on the UI thread
                     final Handler handler = new Handler(Looper.getMainLooper());
-                    handler.postDelayed(() -> {
-                        followingRecyclerViewAdapter.loadMoreItems();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            presenter.loadMoreItems();
+                        }
                     }, 0);
                 }
             }
         }
     }
-
 }
